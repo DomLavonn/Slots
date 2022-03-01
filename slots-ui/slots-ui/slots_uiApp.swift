34e8e2bd0c5d3381525ea53db83b3a58a8988267
DomLavonn <99680793+DomLavonn@users.noreply.github.com>
2022-02-28 21:24:43 -0500

    Added Proj
    added proj

---
+++ slots-ui/slots-ui/slots_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  slots_uiApp.swift
+//  slots-ui
+//
+//  Created by Domanik Johnson on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct slots_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
