50f33c84d66080d4d2a69b3d919610def6c8c922
DomLavonn <99680793+DomLavonn@users.noreply.github.com>
2022-03-01 12:17:36 -0500

    Added code to ContenrView

--- slots-ui/slots-ui/ContentView.swift
+++ slots-ui/slots-ui/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var credits: Int = 0
+    @State var apple: Int = 0
+    @State var cherry: Int = 1
+    @State var star: Int = 2
+
+    
+    
     var body: some View {
-        Text("Hello, world!")
-             .padding()
+        VStack {
+            Spacer()
+            Text("Slots UI!")
+                .font(.largeTitle)
+                .foregroundColor(Color.black)
+                .fontWeight(.bold)
+                 .padding()
+            Spacer()
+            Text("Credits: \(credits)")
+                .font(.title2)
+            Spacer()
+            HStack{
+                Spacer()
+                Image("apple").resizable().scaledToFit()
+                Spacer()
+                Image("cherry").resizable().scaledToFit()
+                Spacer()
+                Image("star").resizable().scaledToFit()
+                Spacer()
+            }
+            Spacer()
+            Button("Spin"){
+                
+            }.font(.title)
+                .foregroundColor(Color.white)
+                .padding(.horizontal, 25.0)
+            .background(Color.pink)
+            .cornerRadius(40)
+
+
+            Spacer()
+        }
+       
     }
 }
 
